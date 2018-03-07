fc6c9453aa6a1a691e8bc02139f15489295467ee
BalaRajendran <31990008+BalaRajendran@users.noreply.github.com>
2018-03-07 20:20:29 +0530

    Add files via upload

---
+++ guvi_1.py
@@ -0,0 +1,10 @@
+print ("Code for adding a number")
+a=input("Enter a Number to check for positive,negative,or Zero : ");
+b=int(a);
+if b==0:
+    print("Zero");
+elif int(a) >= 0:
+    print("Positive");
+else:
+        print("Negative ");
+    
